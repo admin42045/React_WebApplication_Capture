1f911433a548d6b0b37dd7adf450ed123a018d25
admin42045 <nitinkumar42045@gmail.com>
2021-07-20 19:04:07 +0530

    9.Intro Adding routing

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,47 +1,52 @@
-import React from 'react';
+import React from "react";
 
-import styled from 'styled-components';
-
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
-    return(
-        <StyledNav>
-            <h1><a className="logo" href="#">Capture</a></h1>
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Our Work</a></li>
-                <li><a href="#">Contact Us</a></li>
-            </ul>
-        </StyledNav>
-    );
+  return (
+    <StyledNav>
+      <h1>
+        <Link to="/">Capture</Link>
+      </h1>
+      <ul>
+        <li>
+          <Link to="/about">About Us</Link>
+        </li>
+        <li>
+          <Link to="/work">Our Work</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact Us</Link>
+        </li>
+      </ul>
+    </StyledNav>
+  );
 };
 
+const StyledNav = styled.nav`
+  min-height: 10vh;
+  margin: auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 10rem;
 
-const  StyledNav = styled.nav `
-   min-height: 10vh;
-   margin: auto;
-   display: flex;
-   justify-content: space-between;
-   align-items: center;
-   padding:1rem 10rem;
-   
-   a {
-       color: white;
-       text-decoration: none;
-       padding: 2rem;
-       font-size: 1.5rem;
-   }
-
-   ul {
-       display: flex;
-       list-style: none;
-   }
-
-  .logo{
-      font-weight: lighter;
+  a {
+    color: white;
+    text-decoration: none;
+    padding: 2rem;
+    font-size: 1.5rem;
   }
 
+  ul {
+    display: flex;
+    list-style: none;
+  }
+
+  .logo {
+    font-weight: lighter;
+  }
 `;
 
 export default Nav;
