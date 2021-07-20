b72799698c151f79f4693eb9bdc7ea013d20b004
admin42045 <nitinkumar42045@gmail.com>
2021-07-20 16:12:33 +0530

    6. Adding styled in services section

--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -7,59 +7,107 @@ import money from '../img/money.svg';
 import teamwork from '../img/teamwork.svg';
 import home2 from '../img/home2.png';
 
+
+import styled from 'styled-components';
+// import of style.js file
+import { About,Image} from '../style';
 const ServicesSection = () => {
     return(
-        <div className="services">
-            <div className="description">
+        <Services>
+            <Descriptions>
                 <h2>High <span>quality</span> camera photography.</h2>
-                 <div className="cards">
+                 <Cards>
 
 
                     {/* 01. card */}
-                     <div className="card">
+                     <Card>
                          <div className="icon">
                              <img src={clock} alt="clock" />
                              <h3>Effective way.</h3>
                          </div>
-                         <p>A effective way to capture the image of simple way.</p>
-                     </div>
+                         <p>A effective way to of simple way.</p>
+                     </Card>
 
                     {/* 02. card */}
-                     <div className="card">
+                     <Card>
                          <div className="icon">
                              <img src={diaphragm} alt="clock" />
                              <h3>Effective way.</h3>
                          </div>
-                         <p>A effective way to capture the image of simple way.</p>
-                     </div>
+                         <p>A effective way to of simple way.</p>
+                     </Card>
 
                     {/* 03. card */}
-                     <div className="card">
+                     <Card>
                          <div className="icon">
                              <img src={money} alt="clock" />
                              <h3>Effective way.</h3>
                          </div>
-                         <p>A effective way to capture the image of simple way.</p>
-                     </div>
+                         <p>A effective way to of simple way.</p>
+                     </Card>
 
                     {/* 04. card */}
-                     <div className="card">
+                     <Card>
                          <div className="icon">
                              <img src={teamwork} alt="clock" />
                              <h3>Effective way.</h3>
                          </div>
-                         <p>A effective way to capture the image of simple way.</p>
-                     </div>
-
-
-                 </div>
-            </div>
-            <div className="home2">
-                <img src={home2} alt="home 2" />
-            </div>
-
-        </div>
+                         <p>A effective way to of simple way.</p>
+                     </Card>
+                 </Cards>
+            </Descriptions>
+            <Images>
+               <img src={home2} alt="home 2 image" />   
+            </Images>
+        </Services>
     )
 }
+
+
+// styled components
+
+const Services = styled(About) `
+  h2 {
+      padding-bottom: 4rem;
+  }
+
+  p{
+      width: 70%;
+  } 
+`;
+
+const Cards = styled.div `
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Card = styled.div `
+  flex-basis: 15rem;
+  h3 {
+     padding-left: 1.5rem;
+     background: #fff;
+     color: #000;
+     padding:0.2rem 0.4rem;
+  }
+  .icon {
+      display: flex;
+      align-items: center;
+  }
+`;
+
+const Images =styled(Image) `
+   padding: 5rem 0rem 0rem 0rem;
+   img{
+       min-height: 100%;
+       min-width: 100%;
+
+   }
+`;  
+
+const Descriptions = styled.div `
+  flex: 2;
+  padding-right: 4rem;
+`;
+
 
 export default ServicesSection;
